9340a50e3767f4643d7e7e4907327d53ae65291a
sipida <sipida@gmail.com>
2015-10-10 22:04:30 -0700

    Update setup_mac.sh

--- tools/scripts/setup_mac.sh
+++ tools/scripts/setup_mac.sh
@@ -2,3 +2,7 @@
 ln -s "/Applications/Sublime Text.app/Contents/SharedSupport/bin/subl" ~/bin/subl
 # edit .profile, add
 export PATH=$PATH:/Users/$USER/bin
+export EDITOR='subl -w'
+
+# run
+git config --global core.editor "subl -n -w"
